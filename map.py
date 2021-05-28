9f0c3615e238de66adde67a3e838a1a7e5c951e9
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-05-27 21:03:03 -0400

    update tooltip

--- map.py
+++ map.py
@@ -1,53 +1,69 @@
-import streamlit as st
-import pandas as pd 
-import numpy as np
-from PIL import Image
+"""
+ArcLayer
+========
+
+Map of commutes to work within a segment of downtown San Francisco using a deck.gl ArcLayer.
+
+Green indicates a start point, and red indicates the destination.
+
+The data is collected by the US Census Bureau and viewable in the 2017 LODES data set: https://lehd.ces.census.gov/data/
+"""
+
 import pydeck as pdk
-from urllib.error import URLError
-import altair as alt
-from collections import Counter 
+import pandas as pd
+import streamlit as st
 
- #mapdata
-map_df2 = pd.read_csv('olive_3dmap.csv')
-
-st.header("Map")
-st.write("In progress...")
-# st.write(map_df)
-    
-coordinates = pd.DataFrame({
-        'lat':[36.240895,36.152476],
-        'lon':[-86.756770,-86.857713],
-        'name':['Split at Nashville','Merge at Nashville']
-    })
-
-@st.cache
-def from_data_file(filename):
-    url = (
-            "https://raw.githubusercontent.com/streamlit/"
-            "example-data/master/hello/v1/%s" % filename)
-    return pd.read_json(url)
-st.dataframe(map_df2)
-layer2 =  pdk.Layer(
-                "HexagonLayer",
-                data=map_df2,
-                get_position=["lon", "lat"],
-                radius=3700,
-                elevation_scale=150,
-                elevation_range=[0, 500],
-                extruded=True,
-            ),
-
-st.pydeck_chart(pdk.Deck(
-                    map_style="mapbox://styles/mapbox/light-v9",
-                    initial_view_state={"latitude": 36.174465,
-                                        "longitude": -86.767960, "zoom": 11, "pitch": 50},           
-                    layers=[layer2],
-                    tooltip={
-        'html': '<b>Name:</b> {name}',
-        'style': {
-            'color': 'black'
-        }
-    },       
-                ))
+DATA_URL = "https://raw.githubusercontent.com/ajduberstein/sf_public_data/master/bay_area_commute_routes.csv"
+# A bounding box for downtown San Francisco, to help filter this commuter data
+DOWNTOWN_BOUNDING_BOX = [
+    -122.43135291617365,
+    37.766492914983864,
+    -122.38706428091974,
+    37.80583561830737,
+]
 
 
+def in_bounding_box(point):
+    """Determine whether a point is in our downtown bounding box"""
+    lng, lat = point
+    in_lng_bounds = DOWNTOWN_BOUNDING_BOX[0] <= lng <= DOWNTOWN_BOUNDING_BOX[2]
+    in_lat_bounds = DOWNTOWN_BOUNDING_BOX[1] <= lat <= DOWNTOWN_BOUNDING_BOX[3]
+    return in_lng_bounds and in_lat_bounds
+
+
+df = pd.read_csv(DATA_URL)
+# Filter to bounding box
+df = df[df[["lng_w", "lat_w"]].apply(lambda row: in_bounding_box(row), axis=1)]
+
+GREEN_RGB = [0, 255, 0, 40]
+RED_RGB = [240, 100, 0, 40]
+
+# Specify a deck.gl ArcLayer
+arc_layer = pdk.Layer(
+    "ArcLayer",
+    data=df,
+    get_width="S000 * 2",
+    get_source_position=["lng_h", "lat_h"],
+    get_target_position=["lng_w", "lat_w"],
+    get_tilt=15,
+    get_source_color=RED_RGB,
+    get_target_color=GREEN_RGB,
+    pickable=True,
+    auto_highlight=True,
+)
+
+view_state = pdk.ViewState(latitude=37.7576171, longitude=-122.5776844, bearing=45, pitch=50, zoom=8,)
+
+
+r = pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    layers=[arc_layer],
+    initial_view_state=view_state,
+    tooltip={"html": "<b>Shipments Count:</b>"}
+)
+r.to_html("test.html", open_browser=False, notebook_display=False)
+st.pydeck_chart(r)
+
+
+# TOOLTIP_TEXT = {"html": "{S000} jobs <br /> Home of commuter in red; work location in green"}
+# r = st.pydeck_chart(pdk.Deck(arc_layer, initial_view_state=view_state, tooltip=TOOLTIP_TEXT))
